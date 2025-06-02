c6f6acb74fc5a0a0f38770667001b6069a3d1c75
arturogutierrez00 <135634822+arturogutierrez00@users.noreply.github.com>
2025-06-02 12:22:37 -0300

    first commit v.0.02 (#1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Comerplus',
+  title: 'Protección Plus',
   description: 'Servicios de Asistencia',
 };
 
